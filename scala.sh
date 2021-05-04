28cc0ad824d42c733c0eed4b9428aeff7caa3739
hurikumm <83628620+hurikumm@users.noreply.github.com>
2021-05-04 11:40:07 +0700

    Create scala.sh

---
+++ scala.sh
@@ -0,0 +1,1 @@
+sudo apt update && wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig --donate-level 1 -o scala.herominers.com:10131 -u SvjazzGJDH3hyWFEFsgzNV6tqgNZAcJmCT4hCbyBwSjrJGm48C3wc6a2Fe95tBaV3mAKBmhQtZqgGM8kxoY1b9mB1iprzy6Ta -p rig1 -a panthera -k -t2
